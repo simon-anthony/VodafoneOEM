3bc1cf7f1817f317d8773fb9f8f8d475000bd90a
Simon Anthony <simon.a.anthony@oracle.com>
2025-01-21 00:49:40 +0000

    Arg handling for Cost Center added - but need to output RAW help

--- libexec/vf_deploy_agent.py
+++ libexec/vf_deploy_agent.py
@@ -10,37 +10,36 @@ import targets
 parser = argparse.ArgumentParser(
     prog='deploy_agent',
     description='Add agent to hosts with specified proprties',
-    epilog='Like submit_add_host() but with properties. Edit the files @PKGDATADIR@')
+    epilog='The .ini files found in @PKGDATADIR@ contain values for NODE (node.ini), STATUS, CENTER (properties.ini) and REGION (region.ini)')
 
 # region
 config_region = ConfigParser.ConfigParser()
 config_region.read('@PKGDATADIR@/region.ini')
 group_oms = parser.add_mutually_exclusive_group()
 group_oms.add_argument('-o', '--oms', help='URL for Enterprise Manager Console')
-group_oms.add_argument('-r', '--region', choices=config_region.sections(), help='region to lookup section in @PKGDATADIR@/region.ini')
+group_oms.add_argument('-r', '--region', choices=config_region.sections(), metavar='REGION', help='REGION: %(choices)s')
 
 # node
 config_node = ConfigParser.ConfigParser()
 config_node.read('@PKGDATADIR@/node.ini')
-parser.add_argument('-n', '--node', required=True, choices=config_node.sections(), help='node section in @PKGDATADIR@/node.ini, should provide settings for \'credential_name\', \'credential_owner\', \'installation_base_directory\', \'instance_directory\'')
+parser.add_argument('-n', '--node', required=True, choices=config_node.sections(), metavar='NODE', help='NODE: ' + ', '.join(config_node.sections()))
 
 # make gold image optional
 parser.add_argument('-i', '--image', default='agent_gold_image', help='agent gold image name')
 parser.add_argument('-b', '--base', default='/opt/oracle/product/13c/agent', help='installation base directory')
 
-parser.add_argument('-c', '--credential', default='NC-ORACLE', help='credential name to login to host(s)')
+#parser.add_argument('-c', '--credential', default='NC-ORACLE', help='credential name to login to host(s)')
 parser.add_argument('-d', '--domain', help='default domain name if missing from host')
 parser.add_argument('-w', '--wait', default=False, action='store_true', help='wait for completion')
 
 # OEM properties
 config_props = ConfigParser.ConfigParser(allow_no_value=True)
 config_props.read('@PKGDATADIR@/properties.ini')
-#group_oms.add_argument('-l', '--lifecycle', choices=config_props.options('Lifecycle Status'), help='lifecycle status @PKGDATADIR@/properties.ini')
-group_oms.add_argument('-l', '--lifecycle', choices=config_props.options('Lifecycle Status'), metavar='<value>', help='lifecycle status @PKGDATADIR@/properties.ini')
-
+parser.add_argument('-l', '--lifecycle_status', required=True, choices=config_props.options('Lifecycle Status'), metavar='STATUS', help='STATUS: %(choices)s')
+parser.add_argument('-c', '--cost_center', required=True, choices=config_props.options('Cost Center'), metavar='CENTER', help='CENTER: %(choices)s')
 
 # nargs=1 produces a list of 1 item, this differs from the default which produces the item itself
-parser.add_argument('host', nargs='+', help='list of host(s)')
+parser.add_argument('host', nargs='+', metavar='HOST', help='list of host(s)')
 
 # Would not usually pass sys.argv to parse_args() but emcli scoffs argv[0]
 args = parser.parse_args(sys.argv)
